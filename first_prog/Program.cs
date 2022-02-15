132876ae446fcff63c97726dac9398563cb65a6e
Ko1baser <astarikov541@gmail.com>
2022-02-15 10:32:43 +0300

    добавил чилса prosto

--- first_prog/Program.cs
+++ first_prog/Program.cs
@@ -10,7 +10,7 @@ Console.WriteLine($"разность: {chislo - prost}");
 Console.WriteLine($"произведение: {chislo * prost}");
 Console.WriteLine($"деление на цело: {chislo / prost}");
 Console.WriteLine($"возведение в степень: {Math.Pow(chislo, prost)}");
-Console.WriteLine($"корень числа: {Math.Sqrt(chislo)}");
+Console.WriteLine($"корень числа prosto: {Math.Sqrt(chislo)}");
 
 int[] arr = new int[chislo];
 
