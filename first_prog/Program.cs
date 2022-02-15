417a3a9000e7facda753bb7898afcb41d3e84a99
Ko1baser <astarikov541@gmail.com>
2022-02-15 10:15:21 +0300

    Добавьте файлы проекта.

---
+++ first_prog/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите любое число");
+int chislo = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("введите другое число для вычислений");
+int prost = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"сумма: {chislo+prost}");
+Console.WriteLine($"разность: {chislo - prost}");
+Console.WriteLine($"произведение: {chislo * prost}");
+Console.WriteLine($"деление на цело: {chislo / prost}");
+Console.WriteLine($"возведение в степень: {Math.Pow(chislo, prost)}");
+Console.WriteLine($"корень числа: {Math.Sqrt(chislo)}");
+
+int[] arr = new int[chislo];
+
+Random rnd = new Random();
+
+for (int i = 0; i < chislo; i++)
+{
+    arr[i] = rnd.Next(0, 100);
+    Console.Write($"{arr[i]}  ");
+}
